d9a0b1f6bc00d1e580bd7e71afca3a3c175915b8
Your Name <you@example.com>
2026-02-23 16:08:26 +0200

    add static ui parts and style

--- src/components/GridLayout/GridLayout.jsx
+++ src/components/GridLayout/GridLayout.jsx
@@ -3,6 +3,7 @@ import "./GridLayout.css";
 
 const GridLayout = () => {
   const [boxes, setBoxes] = useState([]);
+  const [clicks, setClicks] = useState(0);
 
   useEffect(() => {
     const boxes = new Array(100).fill(0);
@@ -10,11 +11,20 @@ const GridLayout = () => {
   }, []);
 
   return (
-    <div className="grid-layout">
-      {boxes.map((b, index) => {
-       return <div className={"color-box"} key={index}></div>
-      })}
-    </div>
+    <>
+      <h1 className="header">
+        <p className="header-1">Find the</p>
+        <p className="header-2">Secret Color!</p>
+        <div className="secret-color"></div>
+      </h1>
+      <div className="grid-layout">
+        {boxes.map((b, index) => {
+          return <div className={"color-box"} key={index}></div>;
+        })}
+      </div>
+      <section className="clicks-counter">Clicks: {clicks}</section>
+      <footer>Keep Searching!</footer>
+    </>
   );
 };
 
